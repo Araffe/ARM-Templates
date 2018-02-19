2ae443b4f6ad3596f58fa2cfc0f6d7019f6e839a
Adam Raffe <adraffe@microsoft.com>
2018-02-19 11:49:50 +0000

    Creat Users Script

---
+++ infra-security-lab/CreateUsers.ps1
@@ -0,0 +1,12 @@
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Password123."
+$tenant = (Get-AzureADTenantDetail).verifiedDomains.name
+$UPN = "admin@" + $tenant
+$currentuser = (get-azureaduser).userprincipalname.tostring()
+
+if ($currentuser.startswith("admin@", 1)) { New-AzureADUser -DisplayName "Admin" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "admin" -UsageLocation "GB"
+
+$UPN = "Isaiah.Langer@" + $tenant
+New-AzureADUser -DisplayName "Isaiah Langer" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "IsaiahL" -UsageLocation "GB"
+$UPN = "Alex.Wilber@" + $tenant
+New-AzureADUser -DisplayName "Alex Wilber" -PasswordProfile $PasswordProfile -UserPrincipalName $UPN -AccountEnabled $true -MailNickName "AlexL" -UsageLocation "GB"
